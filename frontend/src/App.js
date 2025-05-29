e6893ad9b13f9ec5009121122168d90fcde49503
kaminzhi <kamin@kaminzhi.com>
2025-05-30 06:12:34 +0800

    fix: based up main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Upload from "./components/Upload";
+import Admin from "./components/Admin";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Upload} />
+        <Route path="/admin" component={Admin} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
